ff2ba47e6d86f4c62ee8bba280fc967e7527ab2f
Douglas Berkley <douglasmberkley@gmail.com>
2017-04-06 15:37:56 +0900

    new create destroy all functional

--- Gemfile
+++ Gemfile
@@ -6,6 +6,12 @@ git_source(:github) do |repo_name|
 end
 
 
+gem "better_errors"
+gem "binding_of_caller"
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting ingredients...."
+Ingredient.destroy_all
+puts "shopping for ingredients..."
+
+Ingredient.create(name: "bitters")
+Ingredient.create(name: "brandy")
+Ingredient.create(name: "coke")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "ginger ale")
+Ingredient.create(name: "grenadine")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "juice")
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "soda")
+Ingredient.create(name: "soju")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "vermouth")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "whiskey")
+
+puts "...ready to serve some cocktails"
